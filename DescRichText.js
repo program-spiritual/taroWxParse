1278211fae567dfd1405fd029ea7fe3b5a87b027
huhongyun <xiaomiwujiecao@gmail.com>
2019-01-11 10:48:13 +0800

    taroWxparse 富文本组件封装

---
+++ DescRichText.js
@@ -0,0 +1,79 @@
+import Taro, {Component} from '@tarojs/taro'
+import {
+  View
+}
+  from '@tarojs/components'
+import './DescRichText.scss'
+
+var WxParse = require('./wxParse/wxParse');
+
+export default class DescRichText extends Component {
+  static defaultProps = {
+    isEnable: true
+  }
+  state = {
+
+  }
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      desc: ''
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    console.log('componentWillReceiveProps::',nextProps)
+    var self = this
+
+    self.setState({
+      desc:nextProps.desc
+    })
+  }
+
+  componentWillUpdate() {
+    console.log('componentWillUpdate::',this.state.desc)
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate::',this.state.desc)
+    var self = this
+    var that =  this.$scope
+    if (self.state.desc) {
+      console.log('有内容')
+      var  article = self.state.desc.item.descriptions
+      WxParse.wxParse('article', 'html', article, that, 0)
+    }
+    else {
+      console.log('没有获取到资源')
+    }
+  }
+  componentWillMount() {
+    console.log('componentWillMount::',this.state.desc)
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount::',this.state.desc)
+
+  }
+
+  componentWillUnmout() {
+  }
+
+  componentDidShow() {
+  }
+
+  componentDidHide() {
+  }
+
+  render() {
+
+    return (
+      <View>
+        {/*<RichText nodes={detail.item.extra}></RichText>*/}
+        <import src='./wxParse/wxParse.wxml'/>
+        <template is="wxParse" data="{{wxParseData:article.nodes}}"/>
+      </View>
+    )
+  }
+}
